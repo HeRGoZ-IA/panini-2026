c5effe2c520f3e1ef468b4deab36f8ba06b04d5e
HeRGoZ-IA <helragom@gmail.com>
2026-05-26 23:11:35 -0500

    Update service worker cache version and assets

--- pwa/sw.js
+++ pwa/sw.js
@@ -1,14 +1,27 @@
-const CACHE = 'panini-2026-v1';
+const CACHE = 'cromopro-v2';
 const ASSETS = [
   '/panini-2026/pwa/',
   '/panini-2026/pwa/index.html',
-  '/panini-2026/pwa/manifest.json'
+  '/panini-2026/pwa/manifest.json',
+  '/panini-2026/pwa/icon-72.png',
+  '/panini-2026/pwa/icon-192.png',
+  '/panini-2026/pwa/icon-512.png'
 ];
 
 self.addEventListener('install', e => {
   e.waitUntil(
     caches.open(CACHE).then(c => c.addAll(ASSETS))
   );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
